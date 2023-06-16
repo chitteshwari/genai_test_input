aaec3b7d2af7b105600a4893d831acc31aaa7d14
chitteshwari <80682927+chitteshwari@users.noreply.github.com>
2023-06-16 14:02:26 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_product(a, b):
+    return a * b
+
+result_sum = calculate_sum(2, 3)
+print("Sum:", result_sum)
+
+result_product = calculate_product(2, 3)
+print("Product:", result_product)
