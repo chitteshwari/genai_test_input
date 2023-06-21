08149372bbb9872f7f3f3b63cd80a64f43537a92
chitteshwari <80682927+chitteshwari@users.noreply.github.com>
2023-06-21 13:31:34 +0530

    Update calc_updated.py

--- calc_updated.py
+++ calc_updated.py
@@ -11,8 +11,8 @@ result_product = calculate_product(2, 3)
 print("Product:", result_product)
 
 # New function added
-def calculate_diff(a, b):
-    return a - b
+def calculate_difference(a, b):
+    return a y b
 
 result_difference = calculate_difference(5, 3)
 print("Difference:", result_difference)
