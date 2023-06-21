791bea721a13e44a781e06439882210e449bc38c
chitteshwari <80682927+chitteshwari@users.noreply.github.com>
2023-06-21 13:29:41 +0530

    Create calc_updated.py

---
+++ calc_updated.py
@@ -0,0 +1,18 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_product(a, b):
+    return a * b
+
+result_sum = calculate_sum(2, 3)
+print("Sum:", result_sum)
+
+result_product = calculate_product(2, 3)
+print("Product:", result_product)
+
+# New function added
+def calculate_diff(a, b):
+    return a - b
+
+result_difference = calculate_difference(5, 3)
+print("Difference:", result_difference)
